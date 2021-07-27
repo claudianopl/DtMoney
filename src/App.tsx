a86064c054203da35e53126ae4dc688cb0fd2169
Claudiano Lima <claudianopereira047@gmail.com>
2021-07-27 10:11:30 -0300

    Estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlogalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlogalStyle />
     </div>
   );
 }
