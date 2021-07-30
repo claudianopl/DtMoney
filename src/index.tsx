89d7d5d9510140816ee3d51d5b17eee6f6a2e7ba
Claudiano Lima <claudianopereira047@gmail.com>
2021-07-30 10:50:22 -0300

    Consumo de api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return (
+        {
+          id: 1,
+          description: 'Teste de api',
+          amount: 200,
+        }
+      );
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
